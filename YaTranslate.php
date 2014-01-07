022f9e03d7b96638bbf4cf5cdca5407e23e29447
Aleksandr Tebiev <zzt.tzz@gmail.com>
2014-01-07 17:13:14 +0000

    comments were added

--- YaTranslate.php
+++ YaTranslate.php
@@ -89,8 +89,73 @@ class YaTranslate{
         RETURN $this->send_request($url);
     }
     
+    /**
+     * Getting a list of supported language pairs
+     * .
+     * $ui              - possible values are the language codes like  ru, en, uk, be...
+     *                    if set, the language names are displayed in the language which code corresponding to this parameter.
+     * $callback        - javascript callback function name, used only with json format
+     * 
+     * Response Codes:
+     * 200  - Operation completed successfully
+     * 401  - Wrong API key
+     * 402  - Api key has blocked
+     * 
+     * list of supported languages - http://api.yandex.ru/translate/langs.xml
+     * description in russin - http://api.yandex.ru/translate/doc/dg/reference/getLangs.xml
+     **/ 
     public function get_langs($ui=FALSE,$callback=FALSE){
         RETURN $this->prepare_request('getLangs',array('ui' => $ui, 'callback' => $callback));
     }
 
+    /**
+     * Detects text language
+     * 
+     * $text            - Text which language should be detected
+     * $callback        - javascript callback function name, used only with json format
+     * 
+     * Response Codes:
+     * 200  - Operation completed successfully
+     * 401  - Wrong API key
+     * 402  - Api key has blocked
+     * 403  - Daily limit requests has been exceeded
+     * 404  - Daily amount of translated text has been exceeded
+     * 
+     * Full description in russin - http://api.yandex.ru/translate/doc/dg/reference/detect.xml
+     **/ 
+    public function detect($text=FALSE,$callback=FALSE){
+        RETURN $this->prepare_request('detect',array('text' => $text, 'callback' => $callback));
+    }
+    
+    /**
+     * Translates text to the needed language
+     * 
+     * $text            - Text which should be translated
+     * $lang            - Translate direction.
+     *                    Examples:
+     *                    'ru-en'   - means translate from russian to english
+     *                    or you can put only result language code, like:
+     *                    'en'      - in this way system will try to figure out source language and translate it to english
+     * $format          - result format
+     *                    'plain'   - plain text without markup (defaul value)
+     *                    'html'    - html formatted
+     * $options         - only one option - 1
+     *                    option 1 includes automaticly detected source language into the answer (result)
+     * $callback        - javascript callback function name, used only with json format
+     * 
+     * Response Codes:
+     * 200  - Operation completed successfully
+     * 401  - Wrong API key
+     * 402  - Api key has blocked
+     * 403  - Daily limit requests has been exceeded
+     * 404  - Daily amount of translated text has been exceeded
+     * 413  - Your text too long
+     * 422  - text can not be translated
+     * 501  - translate direction is not supported
+     * 
+     * Full description in russin - http://api.yandex.ru/translate/doc/dg/reference/translate.xml
+     **/ 
+    public function translate($text=FALSE, $lang=FALSE, $format=FALSE, $options=FALSE, $callback=FALSE){
+        RETURN $this->prepare_request('translate',array('text' => $text, 'lang' => $lang, 'format' => $format, 'options' => $options, 'callback' => $callback));
+    }
 }

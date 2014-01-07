e82b7a2c36ffe9dacef509b145f1f053255a983f
Aleksandr Tebiev <zzt.tzz@gmail.com>
2014-01-07 18:25:37 +0000

    First release

--- README.md
+++ README.md
@@ -3,4 +3,27 @@ yandex_translate
 
 Php class for using Yandex translate system
 
-Please don't use it. I am currently in development.
+This system is like Bing Translator or Google translate, but api is free for use and without strict limits.
+     * The API offers text translation features for over 30 languages. 
+     * 
+     * To begin using this class you need to get your own API key, you can do it just in pair of clicks.
+     * Firstly create a Yandex account - https://passport.yandex.com/passport
+     * Finally get your API key - http://api.yandex.ru/key/form.xml?service=trnsl
+     * 
+     * Official api description and documentation in English - http://api.yandex.com/translate/
+     *                                            in russian - http://api.yandex.ru/translate/
+     * 
+     * Example:
+     * 
+     * include "YaTranslate.php";
+     * $key = 'YOUR_API_KEY';
+     * $tr = new YaTranslate($key);
+     * 
+     * $result = $tr->detect('Γεια');                                  //Detects language of the original text
+     * var_dump($result);
+     * 
+     * $result = $tr->translate('Hello, how are you?','en-fr');        //Translate from French to English
+     * ($result);
+     * 
+     * $result = $tr->translate('Bonjour, comment allez-vous?','en');  //Detects language of the original text and translates in English
+     * var_dump($result);
--- YaTranslate.php
+++ YaTranslate.php
@@ -1,4 +1,33 @@
 <?php
+    /**
+     * Yandex Translate
+     * This system is like Bing Translator or Google translate, but api is free for use and without strict limits.
+     * The API offers text translation features for over 30 languages. 
+     * 
+     * To begin using this class you need to get your own API key, you can do it just in pair of clicks.
+     * Firstly create a Yandex account - https://passport.yandex.com/passport
+     * Finally get your API key - http://api.yandex.ru/key/form.xml?service=trnsl
+     * 
+     * Official api description and documentation in English - http://api.yandex.com/translate/
+     *                                            in russian - http://api.yandex.ru/translate/
+     * 
+     * Example:
+     * 
+     * include "YaTranslate.php";
+     * $key = 'YOUR_API_KEY';
+     * $tr = new YaTranslate($key);
+     * 
+     * $result = $tr->detect('Γεια');                                  //Detects language of the original text
+     * var_dump($result);
+     * 
+     * $result = $tr->translate('Hello, how are you?','en-fr');        //Translate from French to English
+     * ($result);
+     * 
+     * $result = $tr->translate('Bonjour, comment allez-vous?','en');  //Detects language of the original text and translates in English
+     * var_dump($result);
+     * 
+     * @author Tebiev Aleksandr | zzt.tzz@gmail.com
+     **/
 class YaTranslate{
     
     private $key                = '';
@@ -6,6 +35,11 @@ class YaTranslate{
     private $request_timeout    = 4;
     private $result_format      = '';
     private $method             = '';
+    
+    function __construct($key, $result_format='json'){
+        $this->set_key($key);
+        $this->set_result_format($result_format);
+    }
 
     public function set_key($key){
         $this->key = $key;
@@ -52,11 +86,6 @@ class YaTranslate{
         //Which one is better?
     }
     
-    function __construct($key, $result_format='json'){
-        $this->set_key($key);
-        $this->set_result_format($result_format);
-    }
-    
     function send_request($url){
         $defaults = array(
             CURLOPT_URL             => $url,
@@ -90,38 +119,46 @@ class YaTranslate{
     }
     
     /**
-     * Getting a list of supported language pairs
-     * .
-     * $ui              - possible values are the language codes like  ru, en, uk, be...
-     *                    if set, the language names are displayed in the language which code corresponding to this parameter.
-     * $callback        - javascript callback function name, used only with json format
+     * Returns a list of translation directions supported by the service.
+     * 
+     * @param string $ui        - possible values are the language codes like  ru, en, uk, be...
+     *                            if set, the language names are displayed in the language which code corresponding to this parameter.
+     * @param string $callback  - javascript callback function name, used only with json format
+     * @return string
      * 
      * Response Codes:
      * 200  - Operation completed successfully
-     * 401  - Wrong API key
-     * 402  - Api key has blocked
+     * 401  - Invalid API key.
+     * 402  - This API key has been blocked.
      * 
      * list of supported languages - http://api.yandex.ru/translate/langs.xml
-     * description in russin - http://api.yandex.ru/translate/doc/dg/reference/getLangs.xml
+     * Description in English   - http://api.yandex.com/translate/doc/dg/reference/getLangs.xml
+     * Description in russin    - http://api.yandex.ru/translate/doc/dg/reference/getLangs.xml
      **/ 
     public function get_langs($ui=FALSE,$callback=FALSE){
         RETURN $this->prepare_request('getLangs',array('ui' => $ui, 'callback' => $callback));
     }
 
     /**
-     * Detects text language
+     * Detects the language of the specified text.
      * 
-     * $text            - Text which language should be detected
-     * $callback        - javascript callback function name, used only with json format
+     * @param string $text      - The text to detect the language for.
+     *                            Restrictions:
+     *                            Although the text parameter is an array (you can pass multiple text parameters),
+     *                            only one language tag is returned for the entire text. To detect the language 
+     *                            of each text item, call the detect method for each of them.
+     * @param string $callback  - javascript callback function name, used only with json format
+     * @return string
      * 
      * Response Codes:
      * 200  - Operation completed successfully
-     * 401  - Wrong API key
-     * 402  - Api key has blocked
-     * 403  - Daily limit requests has been exceeded
-     * 404  - Daily amount of translated text has been exceeded
+     * 401  - Invalid API key.
+     * 402  - This API key has been blocked.
+     * 403  - You have reached the daily limit for requests (including calls of the translate method).
+     * 404  - You have reached the daily limit for the volume of translated text (including calls of the translate method).
      * 
-     * Full description in russin - http://api.yandex.ru/translate/doc/dg/reference/detect.xml
+     * Description in English   - http://api.yandex.com/translate/doc/dg/reference/detect.xml
+     * Description in russin    - http://api.yandex.ru/translate/doc/dg/reference/detect.xml
      **/ 
     public function detect($text=FALSE,$callback=FALSE){
         RETURN $this->prepare_request('detect',array('text' => $text, 'callback' => $callback));
@@ -130,30 +167,38 @@ class YaTranslate{
     /**
      * Translates text to the needed language
      * 
-     * $text            - Text which should be translated
-     * $lang            - Translate direction.
-     *                    Examples:
-     *                    'ru-en'   - means translate from russian to english
-     *                    or you can put only result language code, like:
-     *                    'en'      - in this way system will try to figure out source language and translate it to english
-     * $format          - result format
-     *                    'plain'   - plain text without markup (defaul value)
-     *                    'html'    - html formatted
-     * $options         - only one option - 1
-     *                    option 1 includes automaticly detected source language into the answer (result)
-     * $callback        - javascript callback function name, used only with json format
+     * @param string $text      - Text which should be translated
+     * @param string $lang      - Translation direction (for example, "en-ru" or "ru").
+     *                            Format:
+     *                            1.    A pair of language codes separated by a dash. 
+     *                                  For example, "en-ru" specifies to translate from English to Russian.
+     *                            2.    Single language code. 
+     *                                  For example, "ru" specifies to translate to Russian. 
+     *                                  In this case, the language of the original text is detected automatically.
+     * @param string $format    - Text format.
+     *                            Possible values:
+     *                            "plain"   - Text without markup (default value).
+     *                            "html"    - Text in HTML format.
+     * @param integer $options  - Translation options.
+     *                            Possible values:
+     *                            1 - Automatically detect language. For example, if the lang parameter has reversed 
+     *                            the translation direction for a pair, the service automatically detects the text 
+     *                            language and returns it in the detected tag.
+     * @param string $callback  - javascript callback function name, used only with json format
+     * @return string
      * 
      * Response Codes:
      * 200  - Operation completed successfully
-     * 401  - Wrong API key
-     * 402  - Api key has blocked
-     * 403  - Daily limit requests has been exceeded
-     * 404  - Daily amount of translated text has been exceeded
-     * 413  - Your text too long
-     * 422  - text can not be translated
-     * 501  - translate direction is not supported
+     * 401  - Invalid API key.
+     * 402  - This API key has been blocked.
+     * 403  - You have reached the daily limit for requests (including calls of the detect method).
+     * 404  - You have reached the daily limit for the volume of translated text (including calls of the detect method).
+     * 413  - The text size exceeds the maximum.
+     * 422  - The text could not be translated. 
+     * 501  - The specified translation direction is not supported.
      * 
-     * Full description in russin - http://api.yandex.ru/translate/doc/dg/reference/translate.xml
+     * Description in English   - http://api.yandex.com/translate/doc/dg/reference/translate.xml
+     * Description in russin    - http://api.yandex.ru/translate/doc/dg/reference/translate.xml
      **/ 
     public function translate($text=FALSE, $lang=FALSE, $format=FALSE, $options=FALSE, $callback=FALSE){
         RETURN $this->prepare_request('translate',array('text' => $text, 'lang' => $lang, 'format' => $format, 'options' => $options, 'callback' => $callback));
